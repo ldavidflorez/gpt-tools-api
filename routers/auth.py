0ad983f9a161f5a57650c29a07fd4eca34726810
Luis F <luisflorexpareja@gmail.com>
2023-06-27 11:42:18 -0500

    new origin

--- routers/auth.py
+++ routers/auth.py
@@ -82,7 +82,7 @@ def login_for_access_token(response: Response, form_data: OAuth2PasswordRequestF
     token = create_access_token(
         user.username, user.id, user.role, user.subscription, token_expires, permissions_list)
     response.set_cookie(
-        key="access_token", value=f"Bearer {token}", httponly=True, secure=False, samesite="lax")
+        key="access_token", value=f"Bearer {token}", httponly=True, secure=False, samesite="none")
     return {"access_token": token, "token_type": "Bearer"}
 
 
